5debcab20a42ebaec01aad35cb02eeb32ae78106
josedulanto <jose.dulanto@live.com>
2013-04-16 17:13:12 -0500

    Twitter initializer

---
+++ config/initializers/twitter.rb
@@ -0,0 +1,6 @@
+Twitter.configure do |config|
+  config.consumer_key = '15jkOC7J7l5uAXUZ0g9Q'
+  config.consumer_secret = 'tb7JLtmYW49SyzFouE8lrhYBg4JKpKK3qfa7FM8gk'
+  config.oauth_token = '16396800-SZTW7EwMbHCIMN6rm2qOfGrTpcKOwwfqD21GddC8'
+  config.oauth_token_secret = '51m1OTO4RBLlcMevC3J8b0KK58S70IM3xpO9qV4Ms'
+end
